2f2556c2631953af7738d71d35baad995c2bd1dc
roysegal11 <roysegal11@gmail.com>
2020-07-29 16:53:06 +0300

    exs in class

--- School/Course.py
+++ School/Course.py
@@ -1,20 +1,21 @@
 from School.Students1 import Students1
+from School import Main
 
 class Course:
 
-    def __init__(self, course_num, course_name, subjects_list, students, full):
+    def __init__(self, course_num, course_name, students, full):
         self.course_num = course_num
         self.course_name = course_name
-        self.subjects_list = subjects_list
+        self.subjects_list = []
         self.students = students
         self.full = full
 
 
     def add_student(self, name, ID):
-        return Students1(ID, name)
+        self.subjects_list.append(Students1(ID, name))
 
     def __str__(self):
-        return f'Hello {namenjo}'
+        return f'Hello {Shlomi}'
 
 
 
